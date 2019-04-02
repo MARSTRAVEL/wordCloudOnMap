76e35499c4edfa9ff103513e9ef6fc854a1c2f73
Rauha <yunlong.a.liu@gmail.com>
2019-04-02 14:00:34 +0300

    changes

--- wordCloud.js
+++ wordCloud.js
@@ -245,13 +245,99 @@ function convertCanvasToBase64() {
 }
 
 
-  const infowindow = new google.maps.InfoWindow({
-    position: myLatLng,
-  });
-  marker.setMap(map);
-  marker.addListener('click', () => {
-    infowindow.setContent(convertCanvasToBase64());
-    infowindow.open(map, marker);
+// This example adds hide() and show() methods to a custom overlay's prototype.
+// These methods toggle the visibility of the container <div>.
+// Additionally, we add a toggleDOM() method, which attaches or detaches the
+// overlay to or from the map.
+class WordCloudOverlay extends google.maps.OverlayView {
+  constructor(bounds, image, map) {
+    super(bounds, image, map);
+    this.bounds = bounds;
+    this.image = image;
+    this.map = map;
+    // Define a property to hold the image's div. We'll
+    // actually create this div upon receipt of the onAdd()
+    // method so we'll leave it null for now.
+    this.div = null;
+    // Explicitly call setMap on this overlay
+    this.setMap(map);
+  }
+
+  onAdd() {
+    const div = document.createElement('div');
+    div.style.border = 'none';
+    div.style.borderWidth = '0px';
+    div.style.position = 'absolute';
+
+    // Create the img element and attach it to the div.
+    const img = document.createElement('img');
+    img.src = this.image;
+    img.style.width = '100%';
+    img.style.height = '100%';
+    div.appendChild(img);
+    this.div = div;
+    // Add the element to the "overlayImage" pane.
+    const panes = this.getPanes();
+    panes.overlayImage.appendChild(this.div);
+  }
+
+  draw() {
+    // We use the south-west and north-east
+    // coordinates of the overlay to peg it to the correct position and size.
+    // To do this, we need to retrieve the projection from the overlay.
+    const overlayProjection = this.getProjection();
+    // Retrieve the south-west and north-east coordinates of this overlay
+    // in LatLngs and convert them to pixel coordinates.
+    // We'll use these coordinates to resize the div.
+    const sw = overlayProjection.fromLatLngToDivPixel(this.bounds.getSouthWest());
+    const ne = overlayProjection.fromLatLngToDivPixel(this.bounds.getNorthEast());
+
+    // Resize the image's div to fit the indicated dimensions.
+    this.div.style.left = `${sw.x}px`;
+    this.div.style.top = `${ne.y}px`;
+    this.div.style.width = `${ne.x - sw.x}px`;
+    this.div.style.height = `${sw.y - ne.y}px`;
+  }
+
+  onRemove() {
+    this.div.parentNode.removeChild(this.div);
+  }
+  // Set the visibility to 'hidden' or 'visible'.
+
+  hide() {
+    if (this.div) {
+      // The visibility property must be a string enclosed in quotes.
+      this.div.style.visibility = 'hidden';
+    }
+  }
+
+  show() {
+    if (this.div) {
+      this.div.style.visibility = 'visible';
+    }
+  }
+
+  toggle() {
+    if (this.div) {
+      if (this.div.style.visibility === 'hidden') {
+        this.show();
+      } else {
+        this.hide();
+      }
+    }
+  }
+
+  // Detach the map from the DOM via toggleDOM().
+  // Note that if we later reattach the map, it will be visible again,
+  // because the containing <div> is recreated in the overlay's onAdd() method.
+}
+
+function initMap() {
+  const btn = document.getElementById('btn');
+  console.log(btn);
+  const map = new google.maps.Map(document.getElementById('googleMap'), {
+    zoom: 11,
+    center: { lat: latitude, lng: longitude },
   });
   // latitude +- 0.05  longitude +-0.1
   const bounds = new google.maps.LatLngBounds(
